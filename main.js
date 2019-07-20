7719634c8caed46c56e33473797e418b899b747a
Marc Espín <mespinsanz@gmail.com>
2019-07-20 12:38:31 +0200

    You can now configure it

--- main.js
+++ main.js
@@ -1,13 +1,58 @@
 const prettier = require("prettier")
 
+const default_config =  { semi: false }
+
+
+const prettier_instance = new Plugin({
+  name: "Prettier"
+})
+
+prettier_instance.createData(default_config);
+
+let configuration = prettier_instance.getData();
+
 const prettier_dm = new dropMenu({
-	id:"prettier_dm"
+    id:"prettier_dm"
 });
+
 prettier_dm.setList({
   "button": "Prettier",
   "list":{
-  	"Format":{
-		  click:function(){
+    "Configuration":{
+        click:function(){
+            if(configuration!=undefined) delete configuration.parser;
+            const config_window = new Window({
+                id:Math.random(),
+                content:`
+                    <div class=section1>
+                        <h1>Prettier<h1>
+                        <div class=section1>
+                            <textarea id=textarea_config>${JSON.stringify(configuration)}</textarea>
+                        </div> 
+                        <button class=button1 id=save_config>Save</button1>
+                        <button class=button1 id=restart_config>Restart</button1>
+                    </div> 
+                `
+            })
+            config_window.launch();
+            document.getElementById("save_config").onclick = function(){
+                try{
+                    configuration = JSON.parse(document.getElementById("textarea_config").value);
+                }catch{
+                    new Notification("Prettier","There were an error while parsing the configuration.");
+                }
+                
+                prettier_instance.saveData(configuration);
+            }
+             document.getElementById("restart_config").onclick = function(){
+                document.getElementById("textarea_config").value = JSON.stringify(default_config);
+                configuration = default_config;
+                prettier_instance.saveData(default_config);
+            }
+        }
+    },
+    "Format":{
+          click:function(){
             if(graviton.getCurrentEditor() == null) {
                 new Notification("Issue","Open a file before formatting.");
                 return;
@@ -19,22 +64,26 @@ prettier_dm.setList({
             
             switch(getFormat(path.basename(graviton.getCurrentFile().path))){
                 case"js":
-                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), { semi: false, parser: "babel" }));
+                    configuration.parser = "babel";
+                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), configuration));
                     return;
                 case"css":
-                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), { semi: false, parser: "css" }));
+                    configuration.parser = "css";
+                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), configuration));
                     return;
                 case"html":
-                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), { semi: false, parser: "html" }));
+                    configuration.parser = "html";
+                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), configuration));
                     return;
                 case"markdown":
-                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), { semi: false, parser: "html" }));
+                    configuration.parser = "markdown";
+                    graviton.getCurrentEditor().editor.setValue(prettier.format(editor.getValue(), configuration));
                     return;
                 default:
                     new Notification("Issue","The current language is not supported.");
             }
            
-		  }
-	  }
+          }
+      }
   }
 })
